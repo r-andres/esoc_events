ce9d33e80c3308d60be7ef97e6eb8824c0fc0cb0
randres <rafael.andres@ext.esa.int>
2024-02-22 21:09:02 +0100

    black issues solved

--- src/esoc_events/utils/plnview_parser.py
+++ src/esoc_events/utils/plnview_parser.py
@@ -7,7 +7,7 @@ class PlnViewParser(AbstractParser):
 
     def __init__(self, path: Any) -> None:
         AbstractParser.__init__(self, path)
-        self._all = self.root.findall('.//service_session')
+        self._all = self.root.findall(".//service_session")
         self.reset()
 
     @property
@@ -19,16 +19,24 @@ class PlnViewParser(AbstractParser):
         return self
 
     def satellite(self, name: str) -> Any:
-        return self.__query__(lambda session: session.find('./satellite_id').text == name)
+        return self.__query__(
+            lambda session: session.find("./satellite_id").text == name
+        )
 
     def after(self, isoc_utc: str) -> Any:
-        return self.__query__(lambda session: session.find('./activity_start').text > isoc_utc)
+        return self.__query__(
+            lambda session: session.find("./activity_start").text > isoc_utc
+        )
 
     def before(self, isoc_utc: str) -> Any:
-        return self.__query__(lambda session: session.find('./activity_end').text < isoc_utc)
+        return self.__query__(
+            lambda session: session.find("./activity_end").text < isoc_utc
+        )
 
     def ground_station(self, name: str) -> Any:
-        return self.__query__(lambda session: session.find('./ground_station').text == name)
+        return self.__query__(
+            lambda session: session.find("./ground_station").text == name
+        )
 
     def __query__(self, function: Any) -> Any:
         self._sessions = list(filter(function, self._sessions))
--- tests/test_plnview_parser.py
+++ tests/test_plnview_parser.py
@@ -5,11 +5,15 @@ from esoc_events.utils.plnview_parser import PlnViewParser
 
 def test_plnview_parser() -> None:
 
-    path = Path(__file__).parents[0] / 'data' / 'PLNVIEW_20240222T000000_20250101T000000_20240222T135018_v01-00.PASS-O'
+    path = (
+        Path(__file__).parents[0]
+        / "data"
+        / "PLNVIEW_20240222T000000_20250101T000000_20240222T135018_v01-00.PASS-O"
+    )
     parser = PlnViewParser(path)
     assert len(parser.sessions) == 5605
-    assert len(parser.satellite('JUIC').sessions) == 163
-    assert len(parser.after('2024-08').sessions) == 73
-    assert len(parser.before('2024-10').sessions) == 58
+    assert len(parser.satellite("JUIC").sessions) == 163
+    assert len(parser.after("2024-08").sessions) == 73
+    assert len(parser.before("2024-10").sessions) == 58
 
-    assert len(parser.reset().ground_station('MLG').sessions) == 1026
+    assert len(parser.reset().ground_station("MLG").sessions) == 1026
